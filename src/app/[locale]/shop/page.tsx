61dfd397581017357240d09dcb864c9c213df37e
karim emad <karimkarim20444@gmail.com>
2025-07-14 10:57:14 +0300

    edite

--- src/app/[locale]/shop/page.tsx
+++ src/app/[locale]/shop/page.tsx
@@ -78,7 +78,7 @@ export default function ShopPage() {
             {/* Products Grid/List */}
             <div className="p-4">
               {viewMode === "grid" ? (
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6">
                   {products.map((product: Product) => (
                     <Link
                       key={product.id}
